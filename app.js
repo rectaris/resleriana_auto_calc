ee910debf8acce1f4b3736f819d6b525e476237a
rectaris <furi107k@gmail.com>
2024-11-30 18:23:44 +0900

    added : apikeys

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-// Firebaseの設定
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC5B4BBlvp81nsRxqq_eVXWmujxH7X2OdU",
+  authDomain: "webpage-database-resleriana.firebaseapp.com",
+  projectId: "webpage-database-resleriana",
+  storageBucket: "webpage-database-resleriana.firebasestorage.app",
+  messagingSenderId: "715496776709",
+  appId: "1:715496776709:web:124dac1da21f59a653286c",
+  measurementId: "G-JZ6PLZ9DES"
 };
 
-// Firebaseの初期化
-const app = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = firebase.firestore(app);
 
 // データの取得
